4be4351e5438248b53ace42b1d38beae2101c0ff
Folusho Oladipo <fdoladipo@gmail.com>
2021-10-03 23:47:52 +0100

    Optimize formatWeeklySchedule()

--- src/util/formatWeeklySchedule.ts
+++ src/util/formatWeeklySchedule.ts
@@ -5,8 +5,23 @@ const formatWeeklySchedule = (schedule: WeeklySchedule): FormattedDailySchedule[
   const weekLength = ORDERED_WEEK_DAYS.length
   const formattedSchedule: FormattedDailySchedule[] = []
 
-  // TODO: I think there's a way to flatten this into one while loop that uses two counters to keep track of dayIndex and periodIndex. Inspired by how I flattened the loops in the constructor for Percolation().
-  for (let dayIndex = 0; dayIndex < weekLength; dayIndex++) {
+  /* The most obvious solution for this involved using a nested, two-layer loop, which
+  was what I did initially. Checkout the current file in the commit below for more info:
+    17c565003429afb594ff60f038a471c3faa47f4b
+
+  Those loops practically had O(N) complexity even though they were nested. That was
+  because even though a restaurant can open/close N times a day, a week doesn't vary
+  in its number of days.
+
+  However, I always try to flatten nested loops because they almost always have
+  non-linear worst-case time complexity. That was when I realized that I could use
+  a single while loop instead, as seen below.
+  */
+
+  let dayIndex = 0
+  let timeIndex = 0
+  let hoursText = ""
+  while (dayIndex < weekLength) {
     const day = ORDERED_WEEK_DAYS[dayIndex]
     const capitalizedDay = capitalize(day) as CapitalizedDayName
     const timetable = schedule[day]
@@ -17,41 +32,40 @@ const formatWeeklySchedule = (schedule: WeeklySchedule): FormattedDailySchedule[
         day: capitalizedDay,
         label: CLOSED_STATUS_TEXT,
       }
+      dayIndex++
       continue
     }
 
-    let hoursText = ""
-    let timeIndex = 0
-    // Would have used Array.reduce() but that wouldn't let me halve the
-    // number of iterations by handling two elements in each iteration.
-    while (timeIndex < timetable.length) {
-      if (timeIndex === 0 && timetable[timeIndex].type === "close") {
-        timeIndex++
-        continue
-      }
-
-      const openingTime = timetable[timeIndex]
-      const labelForOpeningTime = getTwelveHourFormat(openingTime.value)
-
-      let closingTime = timetable[timeIndex + 1]
-      if (!closingTime) {
-        const indexOfNextDay = (dayIndex + 1) % weekLength
-        const nextDay = ORDERED_WEEK_DAYS[indexOfNextDay]
-        const nextDayTimetable = schedule[nextDay]
-        closingTime = nextDayTimetable[0]
-      }
-      const labelForClosingTime = getTwelveHourFormat(closingTime.value)
-
-      hoursText = hoursText.length > 0
-        ? `${hoursText}${SINGLE_COMMA} ${labelForOpeningTime} - ${labelForClosingTime}`
-        : `${labelForOpeningTime} - ${labelForClosingTime}`
-
-      timeIndex += 2
+    if (timeIndex === 0 && timetable[timeIndex].type === "close") {
+      timeIndex++
+      continue
     }
 
-    formattedSchedule[dayIndex] = {
-      day: capitalizedDay,
-      label: hoursText,
+    const openingTime = timetable[timeIndex]
+    const labelForOpeningTime = getTwelveHourFormat(openingTime.value)
+
+    let closingTime = timetable[timeIndex + 1]
+    if (!closingTime) {
+      const indexOfNextDay = (dayIndex + 1) % weekLength
+      const nextDay = ORDERED_WEEK_DAYS[indexOfNextDay]
+      const nextDayTimetable = schedule[nextDay]
+      closingTime = nextDayTimetable[0]
+    }
+    const labelForClosingTime = getTwelveHourFormat(closingTime.value)
+
+    hoursText = hoursText.length > 0
+      ? `${hoursText}${SINGLE_COMMA} ${labelForOpeningTime} - ${labelForClosingTime}`
+      : `${labelForOpeningTime} - ${labelForClosingTime}`
+
+    timeIndex += 2
+    if (timeIndex > timetable.length - 1) {
+      formattedSchedule[dayIndex] = {
+        day: capitalizedDay,
+        label: hoursText,
+      }
+      hoursText = ""
+      timeIndex = 0
+      dayIndex++
     }
   }
 
